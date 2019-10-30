e3afa0cdd55bb691cdf0ec3e68d0681873500d4c
DeRon Sutton <dsutton1080@gmail.com>
2019-10-30 11:50:43 -0500

    added signup and splasscreen pages

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("splashScreen")
+def home():
+    return "<h1>At Splash Screen</h1>"
+
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return "<h1>At Home Page</h1>"
+
+@app.route("/login")
+def login():
+    return "<h1>At Login Page</h1>"
+
+@app.route("/signup")
+def signup():
+    return "<h1>At Signup Page</h1>"
 
 
 if __name__ == '__main__':
