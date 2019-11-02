94d05f23ca90006305e7b2d128c842d8351305c1
jace-kline <jace_kline@ku.edu>
2019-11-01 22:12:05 -0500

    So close to getting db working

--- database/setup/script.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-for f in /setup/scripts/*
-do
-    mysql test -e "$(cat $f)"
-done
--- database/spawn_db.py
+++ database/spawn_db.py
@@ -2,8 +2,8 @@ import subprocess
 import time
 
 command1 = "docker build -t database_image ./setup"
-command2 = "docker run -d --rm --name database -p 3306:3306 database_image"
-command3 = "sudo docker exec database /bin/sh -c '/setup/script.sh'"
+command2 = "docker run -p 3306:3306 -d --rm --name database database_image"
+command3 = "sudo docker exec -it database /bin/sh -c '/setup/script.sh'"
 
 
 if subprocess.call(command1, shell=True) != 0:
@@ -14,7 +14,10 @@ time.sleep(10)
 if subprocess.call(command2, shell=True) != 0:
     print("Database container deployed")
 
-time.sleep(30)
+# time.sleep(60)
 
-if subprocess.call(command3, shell=True) != 0:
-    print("Scripts executed")
+# if subprocess.call(command3, shell=True) != 0:
+#     print("Scripts executed")
+
+
+    
