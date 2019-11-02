62eb263170110fbf95eb4fe4b2336f427a78934c
jace-kline <jace_kline@ku.edu>
2019-11-01 23:06:39 -0500

    Updates

--- database/script.bat
+++ database/script.bat
@@ -2,5 +2,5 @@
 mysql -h 127.0.0.1 -P 3306 -uroot -ppassword -e "CREATE DATABASE test;"
 
 FOR %%f in (setup\scripts\*) DO (
-    mysql -h 127.0.0.1 -P 3306 -uroot -ppassword test -e %cat %f%%
+    mysql -h 127.0.0.1 -P 3306 -uroot -ppassword test < %f%
 )
