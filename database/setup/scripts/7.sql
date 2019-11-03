e35a856a650f0bfda717a5bfada384f4ca431a22
jace-kline <jace_kline@ku.edu>
2019-11-02 22:51:27 -0500

    Fixed SQL syntax error

--- database/setup/scripts/7.sql
+++ database/setup/scripts/7.sql
@@ -3,6 +3,6 @@ CREATE TABLE Follows (
     follower_username varchar(255) NOT NULL,
     following_username varchar(255) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (person1_username) REFERENCES Users (username),
-    FOREIGN KEY (person2_username) REFERENCES Users (username)
+    FOREIGN KEY (follower_username) REFERENCES Users (username),
+    FOREIGN KEY (following_username) REFERENCES Users (username)
 );
