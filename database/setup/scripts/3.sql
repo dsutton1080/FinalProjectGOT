b3260b40be896fbae11a666d570559b2197513e4
jace-kline <jace_kline@ku.edu>
2019-11-09 12:19:28 -0600

    Added DB migrations and removed old database directory

--- database/setup/scripts/3.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ForumQuestions (
-    id int UNSIGNED NOT NULL AUTO_INCREMENT,
-    author_username varchar(255) NOT NULL,
-    content TEXT NOT NULL,
-    post_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (author_username) REFERENCES Users (username)
-);
--- database/setup/scripts/7.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Follows (
-    id int UNSIGNED NOT NULL AUTO_INCREMENT,
-    follower_username varchar(255) NOT NULL,
-    following_username varchar(255) NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (follower_username) REFERENCES Users (username),
-    FOREIGN KEY (following_username) REFERENCES Users (username)
-);
