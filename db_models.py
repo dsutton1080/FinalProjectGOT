dc0149ec74ca80f4bedc84f3b06957a2ab964c37
jace-kline <jace_kline@ku.edu>
2019-11-09 11:19:17 -0600

    Updated DB models with foreign keys and timestamps

--- db_models.py
+++ db_models.py
@@ -1,32 +1,31 @@
+from datetime import datetime
 from init import db
 
 class User(db.Model):
     __tablename__ = 'Users'
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(64), index=True)
-    last_name = db.Column(db.String(64), index=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    first_name = db.Column(db.String(64), index=True, nullable=False)
+    last_name = db.Column(db.String(64), index=True, nullable=False)
+    username = db.Column(db.String(64), index=True, primary_key=True, unique=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     school = db.Column(db.String(128))
-    school_grade = db.Column(db.String(64))
-    state = db.Column(db.String(64))
-    forum_questions = db.relationship("ForumQuestion")
-    forum_posts = db.relationship("ForumPost")
-    # messages = db.relationship("Message", foreign_keys=['sender_username', 'receiver_username'])
-    user_posts = db.relationship("UserPost")
-    # follows = db.relationship("Follow", foreign_keys=['follower_username', 'following_username'])
+    school_grade = db.Column(db.String(64), nullable=False)
+    state = db.Column(db.String(64), nullable=False)
+    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
+    forum_questions = db.relationship("ForumQuestion", backref='author', lazy='dynamic')
+    forum_posts = db.relationship("ForumPost", backref='author', lazy='dynamic')
+    user_posts = db.relationship("UserPost", backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 class ForumQuestion(db.Model):
     __tablename__ = 'ForumQuestions'
-    id = db.Column(db.Integer, primary_key=True)
-    author_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    content = db.Column(db.Text())
-    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    forum_posts = db.relationship("ForumPost")
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    author_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
+    forum_posts = db.relationship("ForumPost", backref='question', lazy='dynamic')
 
     def __repr__(self):
         return '<ForumQuestion {}>'.format(self.id)
@@ -44,31 +43,31 @@ class ForumPost(db.Model):
 
 class Message(db.Model):
     __tablename__ = 'Messages'
-    id = db.Column(db.Integer, primary_key=True)
-    sender_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    receiver_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    content = db.Column(db.Text())
-    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    sender_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    receiver_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return '<Message ID {} ({} -> {})>'.format(self.id, self.sender_username, self.receiver_username)
 
 class UserPost(db.Model):
     __tablename__ = 'UserPosts'
-    id = db.Column(db.Integer, primary_key=True)
-    author_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    content = db.Column(db.Text())
-    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    author_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return '<UserPost {} by {}>'.format(self.id, self.author_username)
 
 class Follow(db.Model):
     __tablename__ = 'Follows'
-    id = db.Column(db.Integer, primary_key=True)
-    follower_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    following_username = db.Column(db.String(64), db.ForeignKey('Users.username'))
-    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    follower_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    following_username = db.Column(db.String(64), db.ForeignKey('Users.username'), nullable=False)
+    post_time = db.Column(db.DateTime, index=True, default=datetime.utcnow, nullable=False)
 
     def __repr__(self):
         return '<Follow ID {} ({} -> {})>'.format(self.id, self.follower_username, self.following_username)
