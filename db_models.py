5696aaa3957c04d0c0989b475cba9982dce6984a
jace-kline <jace_kline@ku.edu>
2019-11-24 16:37:03 -0600

    Fixed Primary Key error in User class

--- db_models.py
+++ db_models.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__ = 'Users'
     first_name = db.Column(db.String(64), index=True, nullable=False)
     last_name = db.Column(db.String(64), index=True, nullable=False)
-    username = db.Column(db.String(64), index=True, unique=True, nullable=False)
-    email = db.Column(db.String(120), index=True, unique=True, primary_key=True, nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True, primary_key=True, nullable=False)
+    email = db.Column(db.String(120), index=True, unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     school = db.Column(db.String(128))
     grade = db.Column(db.String(64))
